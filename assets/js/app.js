8949f1da310bfb40582996ffcc35a97ce2095faf
Pete Naish <pete.naish@precedent.co.uk>
2014-07-16 12:33:46 +0100

    housekeeping

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,21 @@
 (function ($) {
-    var $window = $(window),
-        on_resize = function (c, t) {
-        $window.on('resize', function () {
-            clearTimeout(t);
-            t = setTimeout(c, 1000);
-        });
-        return c;
-    };
+    // var $window = $(window),
+    //     on_resize = function (c, t) {
+    //     $window.on('resize', function () {
+    //         clearTimeout(t);
+    //         t = setTimeout(c, 1000);
+    //     });
+    //     return c;
+    // };
 
-    on_resize(function () {
-        if ($window.width() > 1000) {
-            $('.project__title').waypoint('sticky', {
-                wrapper: '<div class="wrapper--sticky">'
-            });
-        } else {
-            $('.project__title').waypoint('unsticky');
-        }
-    })();
+    // on_resize(function () {
+    //     if ($window.width() > 1000) {
+    //         $('.project__title').waypoint('sticky', {
+    //             wrapper: '<div class="wrapper--sticky">'
+    //         });
+    //     } else {
+    //         $('.project__title').waypoint('unsticky');
+    //     }
+    // })();
 
 })(jQuery);
