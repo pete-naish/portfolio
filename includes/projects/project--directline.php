8e232ae69ea476e14eef2d4c77a0ce830a1164cb
Pete Naish <pete.naish@gmail.com>
2015-08-18 20:08:32 +0100

    added directline

---
+++ includes/projects/project--directline.php
@@ -0,0 +1,52 @@
+<article class="project">
+    <h3 class="project__title">Direct Line Brand Bible</h3>
+    <img class="project__image" src="/assets/img/content/project--saatchi__direct-line.png">
+    <div class="project__info">
+        <h4 class="info__label">Project brief</h4>
+        <p class="info__text">To create a single-page, fully-responsive, mobile-first static website to showcase brand and identity guidelines</p>
+        <h4 class="info__label">My role</h4>
+        <p class="info__text">Developing all HTML, CSS, and JavaScript assets for the project</p>
+        <h4 class="info__label">Browser support</h4>
+        <p class="info__text">Latest browsers spanning mobile, tablet, and desktop, including Internet Explorer 9+</p>
+        <ul class="project__tools">
+            <li class="project__tool">
+                <?php 
+                   include($_SERVER['DOCUMENT_ROOT'] . "/includes/icons/icon--html5.php");
+                ?>
+            </li>
+            <li class="project__tool">
+                <?php 
+                   include($_SERVER['DOCUMENT_ROOT'] . "/includes/icons/icon--css3.php");
+                ?>
+            </li>
+            <li class="project__tool">
+                <?php 
+                   include($_SERVER['DOCUMENT_ROOT'] . "/includes/icons/icon--jquery.php");
+                ?>
+            </li>
+            <li class="project__tool">
+                <?php 
+                   include($_SERVER['DOCUMENT_ROOT'] . "/includes/icons/icon--sass.php");
+                ?>
+            </li>
+            <li class="project__tool">
+                <?php 
+                   include($_SERVER['DOCUMENT_ROOT'] . "/includes/icons/icon--bem.php");
+                ?>
+            </li>
+            <li class="project__tool">
+                <?php 
+                   include($_SERVER['DOCUMENT_ROOT'] . "/includes/icons/icon--mixture.php");
+                ?>
+            </li>
+            <li class="project__tool">
+                <?php 
+                   include($_SERVER['DOCUMENT_ROOT'] . "/includes/icons/icon--git.php");
+                ?>
+            </li>
+        </ul>
+    </div>
+    <div class="project__actions">
+        <a class="button--positive" href="/directline/">View front-end templates</a>
+    </div>
+</article>
--- index.php
+++ index.php
@@ -17,6 +17,7 @@
             <?php 
                 include($_SERVER['DOCUMENT_ROOT'] . "/includes/projects/project--milkposter.php");
                 include($_SERVER['DOCUMENT_ROOT'] . "/includes/projects/project--frhank.php");
+                include($_SERVER['DOCUMENT_ROOT'] . "/includes/projects/project--directline.php");
                 include($_SERVER['DOCUMENT_ROOT'] . "/includes/projects/project--bbs.php");
                 include($_SERVER['DOCUMENT_ROOT'] . "/includes/projects/project--central.php");
                 include($_SERVER['DOCUMENT_ROOT'] . "/includes/projects/project--dll.php");
@@ -30,8 +31,7 @@
 
         <script>
         function scrollTo(o,n,t){var e=o.scrollTop,c=n-e,l=0,u=20,a=function(){l+=u;var n=Math.easeInOutQuad(l,e,c,t);o.scrollTop=n,t>l&&setTimeout(a,u)};a()}document.getElementsByClassName("logo")[0].onclick=function(e){e.preventDefault();scrollTo(document.body,0,500)},Math.easeInOutQuad=function(o,n,t,e){return o/=e/2,1>o?t/2*o*o+n:(o--,-t/2*(o*(o-2)-1)+n)};
-        </script>
-        <script>
+        
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
