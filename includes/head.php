4e87771b7d3fb7498e7284f0409cfa9e38c3f98b
Pete Naish <peter.naish@saatchi.co.uk>
2015-08-19 16:53:17 +0100

    adding more blog styling and content

--- includes/head.php
+++ includes/head.php
@@ -17,6 +17,6 @@
 <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
 <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
 <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,700,400italic' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="/assets/css/app.min.css">
 <link rel="alternate" type="application/rss+xml" title="RSS" href="/rss.xml" />
